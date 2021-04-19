05dd7d3439b549032324845eb1ceb0e574ec8cea
louisenje <louisenje@gmail.com>
2021-04-19 11:30:20 +0300

    created a function that gets news headlines,processes result articles and searches for news articles

--- app/request.py
+++ app/request.py
@@ -165,3 +165,64 @@ def process_results(newssource_list):
             Newssource_results.append(Newssource_object)
     return Newssource_results
 
+def get_new_headlines():
+    # source=news_article.get('url')
+    get_Newsheadlines=NEWSHEADLINES.format(api_key)
+
+    with urllib.request.urlopen(get_Newsheadlines)as url:
+        Newsarticle_details_data=url.read()
+        Newsarticle_details_response=json.loads(Newsarticle_details_data)
+
+        newsarticle_object=None
+        if Newsarticle_details_response['articles']:
+            Newssource_results_list=Newsarticle_details_response['articles']
+            newsarticle_object=process_results_article(Newssource_results_list)
+
+            # print(Newsarticle_details_response)
+          
+    return newsarticle_object
+
+def process_results_article(newsarticle_list):
+    '''
+    Function  that processes the Newssource_results and transform them to a list of Objects
+
+    Args:
+        Newssource_list: A list of dictionaries that contain news sources
+
+    Returns :
+       Newssource_results: A list of news objects
+    '''
+    Newsarticle_results = []
+
+    for newsarticle_item in newsarticle_list:
+        # print (Newsarticle_results)
+        id=newsarticle_item.get('id')
+        name=newsarticle_item.get('name')
+        author=newsarticle_item.get('author')
+        title=newsarticle_item.get('title')
+        urlToImage=newsarticle_item.get('urlToImage')
+        description=newsarticle_item.get('description')
+        url=newsarticle_item.get('url')
+        publishedAt=newsarticle_item.get('publishedAt')
+        content=newsarticle_item.get('content')
+        # print(newsarticle_item)
+        if urlToImage:
+            newsarticle_object=Newsarticle(id,name,author,title,urlToImage,description,url,publishedAt,content)
+            Newsarticle_results.append(newsarticle_object)
+    return Newsarticle_results
+
+def search_newsarticle(articlesTitle):
+    News_Article_search_URL=NewsarticleSearch_url.format(articlesTitle,api_key)
+    with urllib.request.urlopen(News_Article_search_URL) as   url:
+        search_Article_data=url.read()
+        search_Article_response=json.loads(search_Article_data)
+
+        search_Article_results=None
+
+        if search_Article_response['articles']:
+            search_Article_list=search_Article_response['articles']
+            search_Article_results=process_results_article(search_Article_list)
+
+    return search_Article_results
+
+

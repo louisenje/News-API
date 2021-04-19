be26cdc3151ba220f7bb355f9d143c801417947c
louisenje <louisenje@gmail.com>
2021-04-19 11:27:52 +0300

    Function  that processes news results and gets news

--- app/request.py
+++ app/request.py
@@ -80,3 +80,88 @@ def get_news_article(source):
             # print(Newsarticle_details_response)
           
     return newsarticle_object
+
+def process_results_article(newsarticle_list):
+    '''
+    Function  that processes the Newssource_results and transform them to a list of Objects
+
+    Args:
+        Newssource_list: A list of dictionaries that contain news sources
+
+    Returns :
+       Newssource_results: A list of news objects
+    '''
+    Newsarticle_results = []
+
+    for newsarticle_item in newsarticle_list:
+        # print (Newsarticle_results)
+        id=newsarticle_item.get('id')
+        name=newsarticle_item.get('name')
+        author=newsarticle_item.get('author')
+        title=newsarticle_item.get('title')
+        urlToImage=newsarticle_item.get('urlToImage')
+        description=newsarticle_item.get('description')
+        url=newsarticle_item.get('url')
+        publishedAt=newsarticle_item.get('publishedAt')
+        content=newsarticle_item.get('content')
+        # print(newsarticle_item)
+        if url:
+            newsarticle_object=Newsarticle(id,name,author,title,urlToImage,description,url,publishedAt,content)
+            Newsarticle_results.append(newsarticle_object)
+    return Newsarticle_results
+
+def search_newsarticle(articlesTitle):
+    News_Article_search_URL=NewsarticleSearch_url.format(articlesTitle,api_key)
+    with urllib.request.urlopen(News_Article_search_URL) as   url:
+        search_Article_data=url.read()
+        search_Article_response=json.loads(search_Article_data)
+
+        search_Article_results=None
+
+        if search_Article_response['articles']:
+            search_Article_list=search_Article_response['articles']
+            search_Article_results=process_results_article(search_Article_list)
+
+    return search_Article_results
+def get_news(category):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_newssource_url=NEWSURL.format(category,api_key)
+    with urllib.request.urlopen(get_newssource_url) as url:
+        get_newssource_data=url.read()
+        get_newssource_response=json.loads(get_newssource_data)
+
+        Newssource_results = None
+
+        if get_newssource_response['sources']:
+            Newssource_results_list=get_newssource_response['sources']
+            Newssource_results=process_results(Newssource_results_list)
+
+    return Newssource_results
+
+def process_results(newssource_list):
+    '''
+    Function  that processes the Newssource_results and transform them to a list of Objects
+
+    Args:
+        Newssource_list: A list of dictionaries that contain news sources
+
+    Returns :
+       Newssource_results: A list of news objects
+    '''
+    Newssource_results = []
+    for newssource_item in newssource_list:
+        id=newssource_item.get('id')
+        name=newssource_item.get('name')
+        description=newssource_item.get('description')
+        url=newssource_item.get('url')
+        category=newssource_item.get('category')
+        language=newssource_item.get('language')
+        country=newssource_item.get('country')
+
+        if name:
+            Newssource_object=Newssource(id,name,description,url,category,language,country)
+            Newssource_results.append(Newssource_object)
+    return Newssource_results
+
